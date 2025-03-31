3a989b4e8b6b6906e2871c3fd0149d2e36bec6bd
wizzySmile <67769003+wizzySmile@users.noreply.github.com>
2025-03-31 21:10:25 +0100

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -9,59 +9,71 @@ Script Purpose:
 =====================================================================
 */
 
-DROP TABLE IF EXISTS bronze_crm_cust_info;
-CREATE TABLE DataWarehouse.bronze_crm_cust_info(
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info
+(
 	cst_id INT,
-	cst_key VARCHAR(50),
-	cst_firstname VARCHAR(50),
-	cst_lastname VARCHAR(50),
-	cst_marital_status VARCHAR(50),
-	cst_gndr VARCHAR(50),
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
 	cst_create_date DATE
 );
 
-DROP TABLE IF EXISTS bronze_crm_prd_info;
-CREATE TABLE DataWarehouse.bronze_crm_prd_info(
-prd_id INT,
-prd_key VARCHAR(50),
-prd_nm VARCHAR(50),
-prd_cost INT,
-prd_line VARCHAR(50),
-prd_start_dt DATE,
-prd_end_dt DATE
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info
+(
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
 );
 
-DROP TABLE IF EXISTS bronze_crm_sales_details;
-CREATE TABLE DataWarehouse.bronze_crm_sales_details(
-sls_ord_num VARCHAR(50),
-sls_prd_key VARCHAR(50),	
-sls_cust_id	INT,
-sls_order_dt DATE,
-sls_ship_dt	DATE,
-sls_due_dt DATE,
-sls_sales INT,
-sls_quantity INT,
-sls_price INT
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details
+(
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT  ,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
 );
 
-DROP TABLE IF EXISTS bronze_erp_cust_az12;
-CREATE TABLE DataWarehouse.bronze_erp_cust_az12(
-CID VARCHAR(50),
-BDATE DATE,
-GEN VARCHAR(50)
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12
+(
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50)
 );
 
-DROP TABLE IF EXISTS bronze_erp_loc_a101;
-CREATE TABLE DataWarehouse.bronze_erp_loc_a101(
-CID VARCHAR(50),
-CNTRY VARCHAR(50)
-);
- 
-DROP TABLE IF EXISTS bronze_erp_px_cat_g1v2;
-CREATE TABLE DataWarehouse.bronze_erp_px_cat_g1v2(
-ID VARCHAR(50),
-CAT VARCHAR(50),
-SUBCAT VARCHAR(50),
-MAINTENANCE VARCHAR(50)
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101
+(
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50)
 );
 
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2
+CREATE TABLE bronze.erp_px_cat_g1v2
+(
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50)
+);
+				
